19893ffe0942c2bb7abc1b683e7a0df7d3fb6fb0
sheshanand <iamsheshanandjha@gmail.com>
2025-03-15 04:40:29 +0000

    changes

--- catalog-service/src/main/java/com/sbms/catalogservice/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/sbms/catalogservice/CatalogServiceApplication.java
@@ -3,9 +3,6 @@ package com.sbms.catalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-* catalog app
-* */
 @SpringBootApplication
 public class CatalogServiceApplication {
 
